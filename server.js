7d9210f5763846c84de63c7eeb9fc36e28aff9f3
Hemant Baghel <84630411+HemantBaghel1@users.noreply.github.com>
2021-12-07 15:31:35 +0530

    final

--- server.js
+++ server.js
@@ -1,60 +1,44 @@
 const mongoose = require('mongoose');
-
 const dotenv = require('dotenv');
-const app = require('./app');
-const { doc } = require('prettier');
 
-dotenv.config({path: './config.env'});
+process.on('uncaughtException', err => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
+dotenv.config({ path: './config.env' });
+const app = require('./app');
 
 const DB = process.env.DATABASE.replace(
-    '<PASSWORD>',
-    process.env.DATABASE_PASSWORD
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
 );
+
 mongoose
-// .connect(process.env.DATABASE_LOCAL, {  
-    .connect(DB, {
- 
-    useNewUrlParser : true,
-    useCreateIndex : true,
-    useFindAndModify : false
-    
-})
-.then( () => console.log('DB connection successfull!'));
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log('DB connection successful!'));
 
-const tourSchema = new mongoose.Schema({ 
-    name: {
-        type: String,
-        required: [true, 'A tour must have a price'],
-        unique: true
-    },
-    rating: {
-        type: Number,
-        default:4.5
-    },
-    price: {
-        type: Number,
-        required: [true, 'A tour must have a price']
-    }
-
-});
-
-const Tour = mongoose.model('Tour',tourSchema);
-
-const testTour= new Tour({
-    name: 'The linkin park',
-    price:497
-});
-
-
-testTour.save().then(doc => {
-    console.log(doc);
-}).catch(err => {
-    console.log('ERROR 🌟 ',err);
-});
-
-// console.log(app.get('env')); //set by express
-// console.log(process.env); // env variables set by node 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`app running on the port ${port}...`);
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+process.on('SIGTERM', () => {
+  console.log('👋 SIGTERM RECEIVED. Shutting down gracefully');
+  server.close(() => {
+    console.log('💥 Process terminated!');
+  });
 });
